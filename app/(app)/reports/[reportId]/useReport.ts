0a546d62c61f9832de7df37b41b7748605420e41
therealcos <0xarowana@gmail.com>
2026-04-30 22:50:24 -1000

    log

--- app/(app)/reports/[reportId]/useReport.ts
+++ app/(app)/reports/[reportId]/useReport.ts
@@ -300,6 +300,8 @@ export const useReport = (reportId: string) => {
         const createResponse = await api.createImage(reportId, request);
         if (!createResponse) throw new Error();
 
+        console.log("UPLOADING");
+
         const uploadResponse = await fetch(createResponse.upload_url, {
           method: "PUT",
           // headers: {
