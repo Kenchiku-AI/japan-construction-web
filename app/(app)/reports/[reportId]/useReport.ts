025f9b419d01751036d3f25ecddb508dae1d526c
therealcos <0xarowana@gmail.com>
2026-05-02 13:34:25 -1000

    fix projects loading

--- app/(app)/reports/[reportId]/useReport.ts
+++ app/(app)/reports/[reportId]/useReport.ts
@@ -36,17 +36,17 @@ export const useReport = (reportId: string) => {
       `${process.env.NEXT_PUBLIC_API_WS_URL}/reports/images/ws`,
     );
 
-    // ws.onopen = () => {
-    //   console.log("✅ WebSocket connected!");
-    // };
+    ws.onopen = () => {
+      console.log("✅ WebSocket connected!");
+    };
 
-    // ws.onmessage = (event) => {
-    //   console.log("📨 Received from server:", event.data);
-    // };
+    ws.onmessage = (event) => {
+      console.log("📨 Received from server:", event.data);
+    };
 
-    // ws.onclose = (event) => {
-    //   console.log("❌ Connection closed:", event.code, event.reason);
-    // };
+    ws.onclose = (event) => {
+      console.log("❌ Connection closed:", event.code, event.reason);
+    };
 
     wsRef.current = ws;
 
