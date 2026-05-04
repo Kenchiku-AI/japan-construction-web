e33a53e42264221a9b8399ae56312b4d82c28299
therealcos <0xarowana@gmail.com>
2026-05-04 11:25:23 -1000

    favicon

--- i18n.ts
+++ i18n.ts
@@ -2,12 +2,12 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import en from "./public/locales/en/common.json";
-// import jp from "./public/locales/jp/common.json";
+import jp from "./public/locales/jp/common.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { common: en },
-    // jp: { common: jp },
+    jp: { common: jp },
   },
   fallbackLng: "en",
   lng: "en",
