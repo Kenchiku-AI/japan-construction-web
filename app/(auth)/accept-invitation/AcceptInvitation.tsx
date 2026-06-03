7537a97e078be226a4e6c1ed996afb7f47cf1a4d
therealcos <0xarowana@gmail.com>
2026-06-03 09:05:38 +0900

    use suspense

---
+++ app/(auth)/accept-invitation/AcceptInvitation.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useApi } from "@/lib/api/ApiContext";
+import {
+  existingUserInvitationTokenKey,
+  invitationTokenKey,
+} from "@/lib/constants";
+import { Loader } from "@/app/ui/Loader";
+import { AxiosError } from "axios";
+import { useTranslation } from "react-i18next";
+import { useModal } from "@/lib/modal/ModalContext";
+
+const AcceptInvitation = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get(invitationTokenKey);
+  const router = useRouter();
+  const { acceptInvitation, setCurrentUser } = useApi();
+  const { t } = useTranslation();
+  const { showModal } = useModal();
+
+  useEffect(() => {
+    if (!token) {
+      router.replace("/login");
+      return;
+    }
+
+    sessionStorage.setItem(existingUserInvitationTokenKey, token);
+
+    (async () => {
+      try {
+        const response = await acceptInvitation({ token });
+
+        if (!response?.success) {
+          throw new Error();
+        } else {
+          router.replace("/");
+        }
+      } catch (error) {
+        const message = parseError(error);
+
+        if (message) {
+          showModal({
+            title: t("error"),
+            subtitle: message,
+          });
+        }
+      }
+    })();
+  }, [token, router]);
+
+  const parseError = (error: any) => {
+    const status = (error as AxiosError).status;
+
+    if (status === 410) {
+      sessionStorage.removeItem(existingUserInvitationTokenKey);
+      return t("expired_invitation");
+    }
+
+    if (status === 409) {
+      sessionStorage.removeItem(existingUserInvitationTokenKey);
+      return t("existing_company_invitation");
+    }
+
+    if (status === 404) {
+      sessionStorage.removeItem(existingUserInvitationTokenKey);
+      return t("invalid_invitation");
+    }
+
+    if (status === 403) return t("wrong_user_invitation");
+
+    if (status === 401) {
+      setCurrentUser(undefined);
+      router.replace("/login");
+      return null;
+    }
+
+    return t("invitation_error_description");
+  };
+
+  return <Loader />;
+};
+
+export default AcceptInvitation;
