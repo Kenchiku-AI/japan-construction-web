0a2c93f2ef97d67b92c17b78e0fa4b7a4fb13ddc
therealcos <0xarowana@gmail.com>
2026-06-09 21:55:05 +0900

    wip

--- app/docs/DocsSidebar.tsx
+++ app/docs/DocsSidebar.tsx
@@ -30,6 +30,11 @@ const Sidebar: FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => {
         />
 
         <ul className="menu bg-base-200 text-base-content min-h-full pb-8 px-4">
+          <div className="lg:hidden flex justify-end py-2">
+            <label htmlFor="docs-sidebar" className="btn btn-circle btn-sm">
+              ✕
+            </label>
+          </div>
           <div style={{ color: fontColor2, marginTop: 10 }}>
             {"ご利用ガイド"}
           </div>
