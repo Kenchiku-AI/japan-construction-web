675a43706d917aec6f3a587e344f6d392048b333
therealcos <0xarowana@gmail.com>
2026-06-09 21:48:32 +0900

    wip

--- app/docs/DocsSidebar.tsx
+++ app/docs/DocsSidebar.tsx
@@ -26,8 +26,10 @@ const Sidebar: FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => {
         <label
           htmlFor="docs-sidebar"
           aria-label="close sidebar"
-          className="drawer-overlay"
-        />
+          className="drawer-overlay btn btn-circle btn-sm"
+        >
+          ☰
+        </label>
 
         <ul className="menu bg-base-200 text-base-content min-h-full pb-8 px-4">
           <div style={{ color: fontColor2, marginTop: 10 }}>
