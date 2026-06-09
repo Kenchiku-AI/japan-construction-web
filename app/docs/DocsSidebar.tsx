d88559557961a232dd298789a9f15cb0ae5bc1f2
therealcos <0xarowana@gmail.com>
2026-06-09 21:44:23 +0900

    wip

--- app/docs/DocsSidebar.tsx
+++ app/docs/DocsSidebar.tsx
@@ -23,6 +23,12 @@ const Sidebar: FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => {
       </div>
 
       <div className="drawer-side flex">
+        <label
+          htmlFor="docs-sidebar"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        />
+
         <ul className="menu bg-base-200 text-base-content min-h-full pb-8 px-4">
           <div style={{ color: fontColor2, marginTop: 10 }}>
             {"ご利用ガイド"}
