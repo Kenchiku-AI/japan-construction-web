de4366dca46f06d5d1e8746ece2fabfc727f9011
therealcos <0xarowana@gmail.com>
2026-06-09 21:11:46 +0900

    fix

--- app/docs/DocsSidebar.tsx
+++ app/docs/DocsSidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar: FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => {
             <SidebarItem name="報告書" path="/docs/reports" />
             <SidebarItem name="写真とタグ" path="/docs/photos-and-tags" />
             <SidebarItem name="プロジェクト" path="/docs/projects" />
-            <SidebarItem name="PDF・Excelダウンロード" path="/docs/exports" />
+            <SidebarItem name="ダウンロード" path="/docs/exports" />
             <SidebarItem name="ゲスト" path="/docs/guests" />
             <SidebarItem name="管理者向け" path="/docs/admin" />
           </div>
