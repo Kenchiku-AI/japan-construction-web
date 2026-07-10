65f640506b0ebb53884a0b7cfa7c0c9748368766
kenchikuai <alex@kenchiku.ai>
2026-07-10 21:07:02 +0900

    wip

--- app/ui/Icons.tsx
+++ app/ui/Icons.tsx
@@ -84,7 +84,7 @@ export const UpChevron = ({ size, color }: IconProps) => (
 
 export const DownChevron = ({ size, color }: IconProps) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-    <path d="M6 9L12 15L18 9" stroke={color} stroke-width="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M6 9L12 15L18 9" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
