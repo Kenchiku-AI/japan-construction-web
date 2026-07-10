7a6bbe7eb065d9b5be6c7189341cd9e68bf10990
kenchikuai <alex@kenchiku.ai>
2026-07-11 07:37:04 +0900

    wip

--- app/(auth)/copy/CopyPageContent.tsx
+++ app/(auth)/copy/CopyPageContent.tsx
@@ -16,7 +16,7 @@ export default function CopyPageContent({
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col mt-20 sm:mt-0 sm:justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen pb-12">
       <div className={styles.content}>
         <Heading
           title={t("copy_code")}
