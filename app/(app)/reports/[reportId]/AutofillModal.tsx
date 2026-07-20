7a671c78309418c09afd99bd8045b708acc2783c
kenchikuai <alex@kenchiku.ai>
2026-07-20 20:38:24 +0900

    wip

--- app/(app)/reports/[reportId]/AutofillModal.tsx
+++ app/(app)/reports/[reportId]/AutofillModal.tsx
@@ -129,13 +129,13 @@ const ConversationRow: FC<ConversationRowProps> = ({
         className="px-3"
         style={{
           transition: "height 0.1s ease-in-out",
-          height: isSelected ? (isMobile ? 160 : 120) : 0,
+          height: isSelected ? (isMobile ? 160 : 80) : 0,
           overflow: "hidden"
         }}
       >
         <Divider />
 
-        <div className="px-3 flex gap-3 flex-col md:flex-row md:items-center">
+        <div className="px-3 py-2 flex gap-3 flex-col md:flex-row md:items-center">
           <div className="datePickerLeft">
             <div className={styles.inputLabel}>
               {t("start_time")}
