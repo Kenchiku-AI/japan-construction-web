b7431e43d7e522d681c032fe34be9dae010c0dff
kenchikuai <alex@kenchiku.ai>
2026-07-21 19:48:41 +0900

    wip

--- app/(app)/reports/[reportId]/ReportDashboard.tsx
+++ app/(app)/reports/[reportId]/ReportDashboard.tsx
@@ -293,25 +293,27 @@ const ReportDashboard: FC<ReportDashboardProps> = ({ reportId }) => {
         )}
       </div>
       <div className={`${cardClass} pb-5`}>
-        <div className="flex w-full justify-between py-1 px-3">
-          {report?.status === ReportStatus.Closed ? (
-            <div className="flex items-center py-2 gap-1">
-              <Close color={fontColor2} />
-              <div style={{ color: fontColor2 }}>
-                {t("report_closed")}
+        {(report?.status === ReportStatus.Closed || !!conversations?.length) && (
+          <div className="flex w-full justify-between py-1 px-3">
+            {report?.status === ReportStatus.Closed ? (
+              <div className="flex items-center py-2 gap-1">
+                <Close color={fontColor2} />
+                <div style={{ color: fontColor2 }}>
+                  {t("report_closed")}
+                </div>
               </div>
-            </div>
-          ) : (
-            <Button
-              variant="tertiary"
-              label={t("autofill_from_chat")}
-              iconLeft={() => <Chat color={buttonColor} />}
-              onClick={() => {
-                setIsAutofillModalShown(true);
-              }}
-            />
-          )}
-        </div>
+            ) : (
+              <Button
+                variant="tertiary"
+                label={t("autofill_from_chat")}
+                iconLeft={() => <Chat color={buttonColor} />}
+                onClick={() => {
+                  setIsAutofillModalShown(true);
+                }}
+              />
+            )}
+          </div>
+        )}
         {!!sortedFields && (
           <>
             <div className="flex flex-col w-full">
