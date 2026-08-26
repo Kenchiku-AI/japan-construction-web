d087b28963122609ea0107cb4a8854c4d2dc6ac4
kenchikuai <alex@kenchiku.ai>
2026-08-26 11:49:30 -1000

    wip

--- app/(app)/companies/[companyId]/forms/FormsList.tsx
+++ app/(app)/companies/[companyId]/forms/FormsList.tsx
@@ -8,11 +8,13 @@ import { Form } from "@/app/ui/Icons";
 interface FormsListProps {
   forms: FormJob[];
   isEmpty?: boolean;
+  onClick: (form: FormJob) => void;
 }
 
 const FormsList: FC<FormsListProps> = ({
   forms,
   isEmpty,
+  onClick,
 }) => {
   const { t } = useTranslation();
 
@@ -26,16 +28,22 @@ const FormsList: FC<FormsListProps> = ({
         return (
           <div key={form.id}>
             {i > 0 && <Divider />}
-            <div className="mx-4 flex items-center justify-between">
+            <div
+              className="mx-4 flex items-center justify-between cursor-pointer hover:opacity-50"
+              onClick={() => onClick(form)}
+            >
               <div style={{ height: 60 }} className="flex items-center gap-4">
                 <Form />
-                <div className="flex flex-col">
-                  <div>{form.name}</div>
-                  <div className={styles.subtitle}>{ }</div>
-                </div>
+
+                <div>{form.name}</div>
               </div>
-            </div>
-          </div>
+              {!!form.description && (
+                <div className={styles.subtitle}>
+                  {`${t("status")}: ${t(form.status)}`}
+                </div>
+              )}
+            </div >
+          </div >
         );
       })}
     </>
