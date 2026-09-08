682862ef82b7748999bd6e80cf78c0c8f98e2ece
kenchikuai <alex@kenchiku.ai>
2026-09-07 16:23:51 -1000

    wip

--- app/(app)/projects/[projectId]/useProject.ts
+++ app/(app)/projects/[projectId]/useProject.ts
@@ -468,9 +468,13 @@ export const useProject = (projectId: string) => {
   );
 
   const refreshCustomObjectsByDefinition = useCallback(async () => {
+    console.log("has proj?", project);
+
     if (!project) return;
 
     const relationshipDefs = project.custom_relationships.map((r) => r.definition);
+
+
     const targetIds = relationshipDefs.map((r) => r.target_custom_object_definition_id);
     const definitionIds = targetIds.filter((id) => id != null);
 
